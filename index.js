75a2435dcb4a33ec25faa58d5da4fa9189003ca0
kailash yadav <kailashyadav1998@gmail.com>
2021-04-13 22:12:31 +0530

    add text file

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 //adding http module reeuire
 let http = require('http');
+let fs = require('fs');
 
+// port & hostname
+let hostname='127.0.0.1';
+let port=3000;
+// Normal file data access
+// let data = fs.readFileSync('sid.txt');
+// console.log(data.toString());
+// console.log('Program end')
+
+// creating a callback function
+fs.readFile('sid.txt', function(err,data){
+    if(err) return console.log('error' +err);
+    console.log(data.toString())
+})
+console.log('End the call back function')
 //creating server
-http.createServer(function(reuest,response){
+
+const server=http.createServer(function(reuest,response){
     //send the http header 200
     //type:plain
     response.writeHead(200,{'Content-Type':'text/html'});
@@ -11,8 +27,9 @@ http.createServer(function(reuest,response){
     // End the server
     response.end();
     //port is linstening 3000
-}).listen(3000);
-console.log('Running at http:3000')
-
+});
+server.listen(port,hostname, function(){
+    console.log(`Server runining at http://${hostname}:${port}/`);
+})
 
 
